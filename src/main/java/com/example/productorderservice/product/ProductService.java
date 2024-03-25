ede06658cf088a83df4b67cc7b2b6deaa9c17549
leeshinbi <tlsql6831@naver.com>
2024-03-25 22:50:42 +0900

    상품 등록 POJO 스트링부트 테스트로 전환하기

---
+++ src/main/java/com/example/productorderservice/product/ProductService.java
@@ -0,0 +1,19 @@
+package com.example.productorderservice.product;
+
+import org.springframework.stereotype.Component;
+
+@Component
+class ProductService {
+
+	private final ProductPort productPort;
+
+	private ProductService(final ProductPort productPort){
+		this.productPort = productPort;
+	}
+
+	public void addProduct(final AddProductRequest request){
+		final Product product = new Product(request.name(), request.price(),
+			request.discountPolicy());
+		productPort.save(product);
+	}
+}
